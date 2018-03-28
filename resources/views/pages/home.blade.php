b73771d4db6142c3751a78771f4bb4eaea652ab0
Araquach <adam@contra-design.com>
2018-03-28 15:54:09 +0100

    Fixed the homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -37,12 +37,20 @@
 		<p>Jakata is a fashion forward, award winning hairdressing salon in Warrington, Cheshire. With a team of twelve talented hairdressers looking after you, you know you're in good hands.</p>
 		<p>Based on Bridge street in Warrington Town Centre the salon is located perfectly to serve the whole of Warrington and surrounding areas.</p>
 		<p>If you're looking for the most fashion forward hairdressing then look no further. Our hairdressing services will far outweigh your expectations plus you'll be blown away by our friendly, approachable staff.</p>
-	</section> 
+	</section>
 
 	<div id="home_news">
-		
-			{{ dd($blogs->first()->paras()->first()->para) }}
-		
+		@foreach($blogs as $blog)
+			<a href="{{ URL::to('blog') }}/{{ $blog->slug }}">
+				<aside class="home_news">
+					<div class="home_news_pic" style="background: url('{{ $blog->paras()->first()->para_pic }}') no-repeat top left rgba(249, 249, 250, 0.8); background-size: cover;"></div>
+					<div class="home_news_copy">
+						<h3 class="newsheader">{{ $blog->title }}</h3>
+						<p class="newspara">{{ limit_news_words($blog->paras()->first()->para) }}&hellip;</p>
+					</div>
+				</aside>
+			</a>
+		@endforeach
 	</div>
 
 </div>
