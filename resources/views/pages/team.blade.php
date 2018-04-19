60e02a73070c008bb20de5843617497610cb0e0d
vagrant <vagrant@homestead>
2018-04-19 04:05:29 +0000

    updated the team page spiel

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -6,7 +6,7 @@
 	'description' => 'The Jakata Hairdressing Team',
 	'keywords' => 'hairdressing team, hairdressers, stylists, Jakata team, Jakata stylists, Jakata hairdressers',
 	'ogtitle' => 'Meet the Team',
-	'ogdescription' => 'The Jakata team comprises of seven skilled stylists, all from different hairdressing backgrounds, all displaying a diverse range of skills.Over the years Jakata has trained and developed a number of people to exceptional standards, whilst taking on already established stylists. This mix of talent creates a dynamic team with stylists well suited to anyone with a desire for a fashion forward look.',
+	'ogdescription' => 'The Jakata team comprises of nine skilled stylists, all from different hairdressing backgrounds, all displaying a diverse range of skills.Over the years Jakata has trained and developed a number of people to exceptional standards, whilst taking on already established stylists. This mix of talent creates a dynamic team with stylists well suited to anyone with a desire for a fashion forward look.',
 	'title' => 'Jakata Salon - Meet the team - Hairdressers in Warrington'
 	])
 	
@@ -18,13 +18,11 @@
 <section id="team">
 	<section id="team_overview">
 		<h1>The Team</h1>
-		<p>The Jakata team comprises of six skilled stylists, all from different hairdressing backgrounds, all displaying a diverse range of skills.</p>
+		<p>The Jakata team comprises of nine skilled stylists, all from different hairdressing backgrounds, all displaying a diverse range of skills.</p>
 		<p>Over the years Jakata has trained and developed a number of people to exceptional standards, whilst taking on already established stylists. This mix of talent creates a dynamic team with stylists well suited to anyone with a desire for a fashion forward
 			look.</p>
-		<p>A year ago the team divided into two when we opened our sister salon <a href="http://www.paulkemphairdressing.com" target="_blank">Paul Kemp Hairdressing</a> due to outgrowing our Bridge Street premises. Both teams continue to grow both in number and
-			skill.</p>
 		<p><em>It's difficult to find a stylist well suited to your needs, take the time to read each staff members profile to find the right fit for you. Prices for each stylist vary dependant on a number of factors (experience, specialised skills, high demand etc.)</em></p>
-		<p><strong>Please call to arrange for a complimentary consultation with any of our team members.</strong></p>
+		<p><strong>Please call to arrange for a free consultation with any of our team members.</strong></p>
 	</section>
 	<!--team_overview-->
 
