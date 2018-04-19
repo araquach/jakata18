143e92218db50a281c8bd716b4e301ed526ebd18
vagrant <vagrant@homestead>
2018-04-19 17:39:42 +0000

    Created the team members table

---
+++ database/migrations/2018_04_19_040935_create_team_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('level');
+            $table->string('para1');
+            $table->string('para2');
+            $table->string('para3');
+            $table->string('fav_style');
+            $table->string('fav_prod');
+            $table->string('price');
+            $table->string('review_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_members');
+    }
+}
