0aefb9032a00bc2c0c6bf2582bf545d7a40ae267
vagrant <vagrant@homestead>
2018-05-14 15:31:53 +0000

    Minor alterations to the copy

--- resources/views/pages/salon.blade.php
+++ resources/views/pages/salon.blade.php
@@ -6,7 +6,7 @@
 	'description' => 'The Salon',
 	'keywords' => 'hairdressing premises, hairdressers, salon, Jakata salon, Jakata hairdressers',
 	'ogtitle' => 'The Salon',
-	'ogdescription' => 'After fourteen years in our Bridge street premises where Jakata began, we finally made the move to our brand new salon on Rylands street at the end of last year. It\'s ultra modern, bright and spacious with fourteen styling stations situated over two floors. Both the team and our clients now feel completely at home in the premises plus we\'ve got loads of room to take on new stylists.',
+	'ogdescription' => 'After fourteen years in our Bridge street premises, we finally made the move to our brand new salon on Rylands street at the end of last year. An ultra modern, bright and spacious salon with fourteen styling stations situated over two floors. Both the team and our clients now feel completely at home in the premises plus we\'ve got loads of room to take on new stylists.',
 	'title' => 'Jakata Salon - The Salon - Hairdressers in Warrington'
 	])
 	
@@ -21,8 +21,8 @@
 	<div id="salon-big-image"></div>
 
 	<div id="salon-copy">
-		<p>After fourteen years in our Bridge street premises where Jakata began, we finally made the move to our brand new salon on Rylands street at the end of last year.</p>
-			<p>It's ultra modern, bright and spacious with fourteen styling stations situated over two floors. Both the team and our clients now feel completely at home in the premises plus we've got loads of room to take on new stylists.</p>
+		<p>After fourteen years in our Bridge street premises, we finally made the move to our brand new salon on Rylands street at the end of last year.</p>
+		<p>An ultra modern, bright and spacious salon with fourteen styling stations situated over two floors. Both the team and our clients now feel completely at home in the premises plus we've got loads of room to take on new stylists!</p>
 		<p><strong>Feel free to come and see us. Have a chat with one of our highly skilled stylists to discuss a new look whilst you check out our amazing salon.</strong></p>
 	</div>
 	<div id="outdoor-image"></div>
