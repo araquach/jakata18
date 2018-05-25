60a99868e4d45d991d7508dc367e842939ed9069
vagrant <vagrant@homestead>
2018-05-25 15:55:30 +0000

    Fixed the typo

--- resources/views/layouts/hairdo.blade.php
+++ resources/views/layouts/hairdo.blade.php
@@ -20,7 +20,7 @@
 			<h1><strong><span class="pink">100 Salons</span><br>One Big Night</strong></h1>
 			<p class="tk-co-headline pink"><strong>Wednesday 20 June 2018</strong></p>
 			<section id="hairdo-image"></section>
-			<p><strong>Jakata</strong> has partnered with <em>Schwarzkopf</em> and <em>Creative Haad</em> to be one of 100 salons taking part in the UK's biggest hair party - and we'd love you to be a part of it!</p>
+			<p><strong>Jakata</strong> has partnered with <em>Schwarzkopf</em> and <em>Creative Head</em> to be one of 100 salons taking part in the UK's biggest hair party - and we'd love you to be a part of it!</p>
 			<p><span class="pink">The Big Hair Do</span> is an annual nationwide in-salon “experience” event where salons across the country get to showcase their offerings and we thought it would be great to get involved - especially as we've recently moved to our brand new salon!</p>
 
 			<p><strong>We only have a limited number of places so read on to find out how to get your VIP invite!</strong></p>
