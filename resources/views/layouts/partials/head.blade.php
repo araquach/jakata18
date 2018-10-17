a7ce6704475bd3365a8ddc23169c158bdec3ef38
Araquach <adam@contra-design.com>
2018-10-17 17:21:56 +0100

    Added Pinterest claim token

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -4,13 +4,15 @@
 	<meta name="description" content="{{ $description or 'Jakata is a fashion forward, award winning hair salon in Warrington, Cheshire. With a team of twelve talented hairdressers looking after you, you know you\'re in good hands. Based on Bridge street in Warrington Town Centre the salon is located perfectly to serve Lymm, Stockton Heath, Culcheth, Great Sankey, Widnes and Runcorn. If you\'re looking for the most fashion forward hairdressing then look no further. Our hairdressing services will far outweigh your expectations plus you\'ll be blown away by our friendly, approachable staff. Jakata, 90/92 Bridge Street, Warrington, WA1 2RF, 01925 242960' }}">
 	<meta name="keywords" content="{{ $keywords or 'hair, hairdressers, hairdressing, hairdressers in Warrington, men\'s hairdresing, fashion colour, brazilian blowdry, wedding hair, Warrington, Lymm, Stockton Heath, Culcheth, Great Sankey, Widnes and Runcorn' }}">
     
-    <meta name="author" content="Contra Design">
+  <meta name="author" content="Contra Design">
 	<meta name="viewport" content="width=device-width" />
     
-    <meta property="og:title" content="{{ $ogtitle or 'Jakata Salon' }}">
-    <meta property="og:description" content="{{ $ogdescription or 'Jakata is a fashion forward, award winning salon in Warrington' }}">
+  <meta property="og:title" content="{{ $ogtitle or 'Jakata Salon' }}">
+  <meta property="og:description" content="{{ $ogdescription or 'Jakata is a fashion forward, award winning salon in Warrington' }}">
 	<meta property="og:image" content="{{ $ogimage or url('/') . '/images/fbmeta/main.jpg' }}">
 	<meta property="og:url" content="{{ url()->current() }}">
+
+  <meta name="p:domain_verify" content="251064bc74a96117720a74689c79c2e5"/>
 
 	{{-- Google Analytics --}}
 	<script>
