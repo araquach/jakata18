a569a2450d0605ac69215368d4f32c35eb7693b7
vagrant <vagrant@homestead>
2019-02-08 16:58:05 +0000

    Got the Offers pages nearly done

--- resources/views/pages/offers/abbi.blade.php
+++ resources/views/pages/offers/abbi.blade.php
@@ -5,8 +5,9 @@
 @include('layouts.partials.head', [
 	'description' => '',
 	'keywords' => 'Jakata Offers, Jakata offers, hairdressing Offers, salon Offers, New Year offers',
-	'ogtitle' => '',
-	'ogdescription' => '',
+	'ogtitle' => 'Special introductory offer with our recently promoted stylist Abbi',
+	'ogdescription' => 'Abbi is flying up the ranks and the quality of her work is second to none! Take advantage of 20% off with her this month',
+	'ogimage' => url('/') . '/images/offer/individual/abbi_montage_meta.jpg',
 	'title' => 'Jakata Salon - Abbi Offer - Hairdressers in Warrington'
 	])
 	
@@ -19,7 +20,7 @@
 	<div class="ind-offer-copy">
 		<h1>Special introductory offer with our recently promoted stylist Abbi</h1>
 		<p>Abbi is flying up the ranks and the quality of her work is second to none! Check out some of her recent looks in the pics!</p>
-		<p><strong>If you're considering a new stylist then take advantage of this great intro offer with her&hellip;</strong></p>
+		<p><strong>If you're considering a new stylist then take advantage of this great offer with her&hellip;</strong></p>
 	</div>
 
 	<div id="ind-offer-img-abbi"></div>
@@ -27,6 +28,10 @@
 	<div class="ind-offer-copy">
 		<p><strong>Come in for a FREE consultation and if you like the ideas Abbi comes up with then book in to get 20% off your visit!</strong></p>
 		<p><strong>Call 01925 242960 to book</strong><br>or use our online booking (link at top of page)</p>
+
+		<p>Just mention the offer at the time of your visit</p>
+		<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
+
 		<p class="link"><a href="{{ URL::to('team/abbi') }}" target="_blank">Click here to find out more about her &gt;</a></p>
 	</div>
 
--- resources/views/pages/offers/lucy.blade.php
+++ resources/views/pages/offers/lucy.blade.php
@@ -7,6 +7,7 @@
 	'keywords' => 'Jakata Offers, Jakata offers, hairdressing Offers, salon Offers, New Year offers',
 	'ogtitle' => '',
 	'ogdescription' => '',
+	'ogimage' => url('/') . '/images/offer/individual/lucy_montage_meta.jpg',
 	'title' => 'Jakata Salon - Lucy Offer - Hairdressers in Warrington'
 	])
 	
@@ -27,6 +28,10 @@
 	<div class="ind-offer-copy">
 		<p><strong>Come in for a FREE consultation and if you like the ideas Lucy comes up with then book in to get 20% off your visit!</strong></p>
 		<p><strong>Call 01925 242960 to book</strong><br>or use our online booking (link at top of page)</p>
+
+		<p>Just mention the offer at the time of your visit</p>
+		<p class="small">(Skin Test required 48 hrs before any colour service for new clients)</p>
+
 		<p class="link"><a href="{{ URL::to('team/lucy') }}" target="_blank">Click here to find out more about her &gt;</a></p>
 	</div>
 
