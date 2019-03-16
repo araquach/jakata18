527687223036fb698d9cc13d66840b4fc932feba
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-03-16 16:24:25 +0000

    Update head.blade.php

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,22 +1,26 @@
 <head>
 	
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="{{ $description or 'Jakata is a fashion forward, award winning hair salon in Warrington, Cheshire. With a team of twelve talented hairdressers looking after you, you know you\'re in good hands. Based on Bridge street in Warrington Town Centre the salon is located perfectly to serve Lymm, Stockton Heath, Culcheth, Great Sankey, Widnes and Runcorn. If you\'re looking for the most fashion forward hairdressing then look no further. Our hairdressing services will far outweigh your expectations plus you\'ll be blown away by our friendly, approachable staff. Jakata, 90/92 Bridge Street, Warrington, WA1 2RF, 01925 242960' }}">
 	<meta name="keywords" content="{{ $keywords or 'hair, hairdressers, hairdressing, hairdressers in Warrington, men\'s hairdresing, fashion colour, brazilian blowdry, wedding hair, Warrington, Lymm, Stockton Heath, Culcheth, Great Sankey, Widnes and Runcorn' }}">
     
   <meta name="author" content="Contra Design">
-	<meta name="viewport" content="width=device-width" />
     
   <meta property="og:title" content="{{ $ogtitle or 'Jakata Salon' }}">
   <meta property="og:description" content="{{ $ogdescription or 'Jakata is a fashion forward, award winning salon in Warrington' }}">
 	<meta property="og:image" content="{{ $ogimage or url('/') . '/images/fbmeta/main.jpg' }}">
-  <meta property="og:image:width" content="{{ $ogimagewidth or '1025' }}">
-  <meta property="og:image:height" content="{{ $ogimageheight or '1025' }}">
+  <meta property="og:image:width" content="{{ $ogimagewidth or '1200' }}">
+  <meta property="og:image:height" content="{{ $ogimageheight or '628' }}">
 	<meta property="og:url" content="{{ url()->current() }}">
 
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Pintrest -->
   <meta name="p:domain_verify" content="251064bc74a96117720a74689c79c2e5"/>
 
-	{{-- Google Analytics --}}
+  <!--Google analytics -->
 	<script>
   		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -30,8 +34,6 @@
   <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" />
 	<link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 640px), only screen and (min-device-width : 719px) and (max-device-width : 721px) and (-webkit-min-device-pixel-ratio : 2) and (orientation : portrait)" href="{{ mix('css/mob.css') }}" />
   <link rel="stylesheet" href="https://use.typekit.net/wun6xai.css">
-
-  <script type="text/javascript" src="{{ URL::asset('scripts/mootools.js') }}"></script>
 	
 	{{-- Phorest online booking styling --}}
 	<style>
