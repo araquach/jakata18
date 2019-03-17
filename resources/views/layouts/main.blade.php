a42a3b658df7aea0c01b40c021e559a23983aa74
Araquach <adam@contra-design.com>
2019-03-17 14:58:32 +0000

    More styling

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,52 +8,43 @@
 <body>
 	<div id="main">
 		<div class="main container">
-			<header>
-				<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-					<div class="navbar-brand is-hidden-desktop">
-						<a href="{{ URL::to('/') }}" class="navbar-item">
-							<img src="{{ asset('images/main/jakata_logo_white.png') }}">
-						</a>
-						<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-							<span aria-hidden="true"></span>
-							<span aria-hidden="true"></span>
-							<span aria-hidden="true"></span>
-						</a>
-					</div>
+			<header class="columns">
+				<div class="column is-two-thirds-desktop">
+					<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+						<div class="navbar-brand is-hidden-desktop">
+							<a href="{{ URL::to('/') }}" class="navbar-item">
+								<img src="{{ asset('images/main/jakata_logo_white.png') }}">
+							</a>
+							<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+								<span aria-hidden="true"></span>
+								<span aria-hidden="true"></span>
+								<span aria-hidden="true"></span>
+							</a>
+						</div>
 
-					<div id="navbarBasicExample" class="navbar-menu">
-						<div class="navbar-start">
-							{!! link_to('/', 'Home', ['class' => 'navbar-item']) !!}
-							{!! link_to('recruitment', 'Recruitment', ['class' => 'navbar-item']) !!}
-							{!! link_to('team', 'Team', ['class' => 'navbar-item']) !!}
-							{!! link_to('prices', 'Prices', ['class' => 'navbar-item']) !!}
-							{!! link_to('blog', 'Blog', ['class' => 'navbar-item']) !!}
-							{!! link_to('details', 'Contact', ['class' => 'navbar-item']) !!}
+						<div id="navbarBasicExample" class="navbar-menu">
+							<div class="navbar-start">
+								{!! link_to('/', 'Home', ['class' => 'navbar-item']) !!}
+								{!! link_to('recruitment', 'Recruitment', ['class' => 'navbar-item']) !!}
+								{!! link_to('team', 'Team', ['class' => 'navbar-item']) !!}
+								{!! link_to('prices', 'Prices', ['class' => 'navbar-item']) !!}
+								{!! link_to('blog', 'Blog', ['class' => 'navbar-item']) !!}
+								{!! link_to('details', 'Contact', ['class' => 'navbar-item']) !!}
 
-							<div class="navbar-item has-dropdown is-hoverable">
-								<a class="navbar-link">
-									More
-								</a>
-								<div class="navbar-dropdown">
-									{!! link_to('reviews', 'Reviews', ['class' => 'navbar-item']) !!}
-									{!! link_to('prices', 'Prices', ['class' => 'navbar-item']) !!}
+
+								<div class="navbar-item has-dropdown is-hoverable">
+									<a class="navbar-link">
+										More
+									</a>
+									<div class="navbar-dropdown">
+										{!! link_to('reviews', 'Reviews', ['class' => 'navbar-item']) !!}
+										{!! link_to('prices', 'Prices', ['class' => 'navbar-item']) !!}
+									</div>
 								</div>
 							</div>
 						</div>
-						<div class="navbar-end">
-							<div class="navbar-item">
-								<div class="buttons">
-									<a href="{!! URL::to('details') !!}" class="button is-primary">
-										Contact
-									</a>
-									<a class="button is-light" href="{!! URL::to('booking') !!}">
-										Book Now
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</nav>
+					</nav>
+				</div>
 			</header>
 		</div>
 	</div>
