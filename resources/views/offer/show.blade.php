c834be9ecafc55d9b2fd4aef4a92ad2b1d374304
Adam Carter <adam@contra-design.com>
2020-01-18 11:20:18 +0000

    Finished the NY Lapsed Offer Page

--- resources/views/offer/show.blade.php
+++ resources/views/offer/show.blade.php
@@ -24,7 +24,7 @@
 		<h1>New Year Offer for {{ $offer->first_name }} {{ $offer->last_name}}</h1>
 		<p>Hi {{ $offer->first_name }}, we've not seen you in Jakata for a while and we'd love to have you back!</p>
 		<p>We have a great team of talented stylists at different levels so there's definitely a price to suit you.</p>
-		<p>Alternatively, if you're on a tight budget after Christmas then why not give our new <strong>Base Hair Academy</strong> a go<br>
+		<p>If you're on a tight budget after Christmas then why not give our new <strong>Base Hair Academy</strong> a go?
 			An amazing team of Junior Stylists & Graduates</p>
 		<p>Take advantage of the offers there by clicking the link: <a href="https://basehairdressing.com/offers" target="_blank">Base Hairdressing Offers</a></p>
 		<p>Or Call 01925 242960 to take advantage of the offer below - Just quote offer NY20</p>
@@ -33,8 +33,9 @@
 	<section id="offer-ny-lapsed"></section>
 
 	{!! link_to('booking', "Book now", array('class'=>'book_button')) !!}
-
-	@include('offer._form')
+	<div class="offer-form">
+		@include('offer._form')
+	</div>
 
 </section> <!--end #offers-->
 
