f0223574df87a18b32915b060998a8936b0675f1
Adam Carter <adam@contra-design.com>
2020-05-27 15:36:16 +0100

    Update corona.blade.php

--- resources/views/pages/corona.blade.php
+++ resources/views/pages/corona.blade.php
@@ -27,8 +27,9 @@
         <p>As it stands we have a potential reopening date of the 4th July. This could change as it isn’t set in stone. We are not currently taking any bookings until we have our confirmed dates and our staff rota’s in place. We are unsure yet what our guidelines will be on working within the salon so we may have to re-jig things.</p>
         <p>In the mean time we will be shortly posting a page where you can register to be contacted for an appointment for when we do open the diary so we can get everyone booked in. We can see who has missed appointments and you will be contacted accordingly, but by all means please add your name to the list to confirm you want to get in as we understand some people may wish to wait still and see what the situation is like closer to the time.</p>
         <p><em>Please bare with us and know we will do our best to get everyone in in the shortest time possible, but we are expecting high demand upon reopening. With potential restricted hours per stylist we will do our upmost to accommodate you.</em></p>
-        <p>Thanks so much to all of our clients for being so patient and understanding that this situation is out of all of our hands. We cannot wait to get back to doing what we love best! Please revisit this page for updates and to find out when our registration form is live.</p>
-        <p><strong>We hope to see you all very soon, we miss you! xx</strong></p>
+        <p>Thanks so much to all of our clients for being so patient and understanding that this situation is out of all of our hands. We cannot wait to get back to doing what we love best!</p> 
+        <p>Please revisit this page for updates and to find out when our registration form is live.</p>
+        <h3>We hope to see you all very soon, we miss you! xx</h3>
     </div>
 </div>
 
